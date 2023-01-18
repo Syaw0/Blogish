690fef6decfe313e6f8442de774a173448ed7b0a
Siavash <siamhb7@protonmail.com>
2023-01-18 20:37:23 +0330

    Set Fonts.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,27 @@
 import "@/styles/globals.css";
+import { Arvo, Lato } from "@next/font/google";
 import type { AppProps } from "next/app";
 import style from "../styles/themeToken.module.css";
 
+const arvo = Arvo({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  style: ["italic", "normal"],
+  variable: "--font-head",
+  preload: false,
+});
+
+const lato = Lato({
+  subsets: ["latin"],
+  weight: ["400", "700", "100", "300", "900"],
+  style: ["italic", "normal"],
+  variable: "--font-sub",
+  preload: false,
+});
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={style.color_light}>
+    <div className={`${style.color_light} ${lato.variable} ${arvo.variable}`}>
       <Component {...pageProps} />
     </div>
   );
